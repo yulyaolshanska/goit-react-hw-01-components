db0ca9043d98dfa4e1e2377880290cb0a41cd037
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-06-30 19:48:24 +0300

    Стилизация
    1. Стилизация Статистики.
    2. Рандомный цвет для блоков статистики.
    3. Стилизация Профиля.
    4. Стилизация списка друзей.

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -19,15 +19,15 @@ export default function Profile( {username,
   </div>
 
   <ul className={css.stats}>
-    <li>
+    <li className={css.item}>
       <span className={css.label}>Followers</span>
       <span className={css.quantity}>{stats.followers}</span>
     </li>
-    <li>
+    <li className={css.item}>
       <span className={css.label}>Views</span>
       <span className={css.quantity}>{stats.views}</span>
     </li>
-    <li>
+    <li className={css.item}>
       <span className={css.label}>Likes</span>
       <span className={css.quantity}>{stats.likes}</span>
     </li>
--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -9,10 +9,14 @@ export default function Statistics({stats, title }) {
   <h2  className={css.title}>{title}</h2>
 
         <ul className={css.statList}>
-          {stats.map(({id, label, percentage}) =>  <li key={id} className={css.item}>
-            <span className={css.label}>{label}</span>
-      <span  className={css.percentage}>{percentage}%</span>
-    </li>)}
+          {stats.map(({ id, label, percentage }) => {
+            const randomBcgCol = {backgroundColor: getRandomHexColor()} ;
+
+           return (<li style={randomBcgCol} key={id} className={css.item}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>)
+          })}
   </ul>
 </section>)
 }
@@ -26,4 +30,10 @@ Statistics.propTypes = {
     }
   )),
   title: PropTypes.string.isRequired
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
