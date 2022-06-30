139923cb0ea7eecba95143adb2e9adbd0538f753
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-06-30 13:20:11 +0300

    Структура и стартовая разметка

---
+++ src/components/FriendList/index.jsx
@@ -0,0 +1,6 @@
+export default function FriendList() {
+    return (
+        <ul class="friend-list">
+ 
+    </ul>)
+}
---
+++ src/components/FriendListItem/index.jsx
@@ -0,0 +1,8 @@
+export default function FriendListItem() {
+    return (
+        <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+    </li>)
+}
